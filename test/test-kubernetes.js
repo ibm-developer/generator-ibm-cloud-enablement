c55b10206ac41524fb4fd2df3c6bf785c88eab11
akihikokuroda <akuroda@us.ibm.com>
2017-08-29 11:21:56 -0400

    combine if statements and add some tests

--- test/test-kubernetes.js
+++ test/test-kubernetes.js
@@ -147,6 +147,9 @@ describe('cloud-enablement:kubernetes', () => {
 				it('does not have a Jenkinsfile', () => {
 					assert.noFile('Jenkinsfile');
 				});
+				it('does not have a istio.yaml', () => {
+					assert.noFile('istio.yaml');
+				});
 			}
 		});
 	});
@@ -201,6 +204,7 @@ describe('cloud-enablement:kubernetes', () => {
 			assert.noFile(chartLocation + '/values.yaml');
 			assert.noFile(chartLocation + '/Chart.yaml');
 			assert.noFile('Jenkinsfile');
+			assert.noFile('istio.yaml');
 			assert.noFile('manifests/kube.deploy.yml');
 			
 		});
@@ -221,6 +225,7 @@ describe('cloud-enablement:kubernetes', () => {
 			assert.file(chartLocation + '/values.yaml');
 			assert.file(chartLocation + '/Chart.yaml');
 			assert.file('Jenkinsfile');
+			assert.file('istio.yaml');
 			assert.file('manifests/kube.deploy.yml');
 			
 		});
