ffb4c60ec85fe899241c94bbff786fa4dfcacea2
Mike Tunnicliffe <mike.tunnicliffe@uk.ibm.com>
2017-09-21 15:44:43 +0000

    fix: add .build-ubuntu to swift cfignore

--- generators/deployment/index.js
+++ generators/deployment/index.js
@@ -94,7 +94,7 @@ module.exports = class extends Generator {
 		this.manifestConfig.buildpack = 'swift_buildpack';
 		this.manifestConfig.command = this.name ? (`${this.name}`) : undefined;
 		this.manifestConfig.memory = this.manifestConfig.memory || '128M';
-		this.cfIgnoreContent = ['.build/*', 'Packages/*'];
+		this.cfIgnoreContent = ['.build/*', '.build-ubuntu/*', 'Packages/*'];
 	}
 
 	_configureJavaCommon() {
