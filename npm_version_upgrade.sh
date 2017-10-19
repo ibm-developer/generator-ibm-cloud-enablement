ffad3c44d3b6f307efd72bb3b230eb877664ae7a
Aerex <nanaus13@yahoo.com>
2017-10-19 15:09:41 -0500

    added update semantic version logic;

---
+++ npm_version_upgrade.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -ev
+
+echo "Auto version updating script : version 0.0.1"
+echo "Checking if a new version update is required ..."
+PKG_NAME=`node -e "console.log(require('./package.json').name);"`
+PKG_VER=`node -e "console.log(require('./package.json').version);"`
+NPM_VER=`npm show $PKG_NAME version`
+echo "$PKG_NAME : version = $PKG_VER, npm version = $NPM_VER"
+
+if [[ $TRAVIS_BRANCH = "development" ]]; then
+    echo "Build targetting development - checking if this is a PR or not"
+    if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]]; then
+        echo "This is a build on development, performing additional steps"
+        if [[ $PKG_NAME == $NPM_VER ]]; then
+             echo "Version numbers match, so changing version and committing changes"
+             ./prerelease.sh
+            retval=$?
+            if [[ $retval != 0 ]]; then
+                exit $retval
+            fi
+        fi
+    fi
+fi
---
+++ prerelease.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+echo "Running pre-release checks and scans"
+echo "Determining current version"
+CURRENT_PKG_VER_MAJOR=`node -e "console.log(require('./package.json').version.split('.')[0]);"`
+CURRENT_PKG_VER_MINOR=`node -e "console.log(require('./package.json').version.split('.')[1]);"`
+CURRENT_PKG_VER_FIX=`node -e "console.log(require('./package.json').version.split('.')[2]);"`
+
+git config user.email "travisci@travis.ibm.com"
+git config user.name "Travis CI"
+git config push.default simple
+
+npm run version
+
+echo "Determining next version"
+PKG_VER_NEXT=`node -e "console.log(require('./package.json').version);"`
+NEXT_PKG_VER_MAJOR=`node -e "console.log(require('./package.json').version.split('.')[0]);"`
+NEXT_PKG_VER_MINOR=`node -e "console.log(require('./package.json').version.split('.')[1]);"`
+NEXT_PKG_VER_FIX=`node -e "console.log(require('./package.json').version.split('.')[2]);"`
+
+echo "Creating git branch"
+BRANCH="updateTo${PKG_VER_NEXT}"
+git checkout -b $BRANCH
+
+if [[ $CURRENT_PKG_VER_MAJOR !=  $NEXT_PKG_VER_MAJOR ]]; then
+    echo "Major version change detected, running OSS scan"
+    npm run changelog
+    if [ $? != 0 ]; then
+        echo "WARNING : scan failed, see logs for more details"
+    fi
+
+
+MSG="[Travis - npm version update] Increment version to ${PKG_VERS_NEXT}"
+
+git status
+git commit -m "${MSG}"
+#this branch will need to be reviewed and approved in the usual manner
+git push --follow-tags origin $BRANCH
