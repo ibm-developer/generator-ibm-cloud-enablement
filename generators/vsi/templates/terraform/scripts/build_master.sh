3bfc63e9f1724a317c3a05563c54188478c298e2
Wilson Ding <hello@wilsonding.com>
2018-07-06 13:48:08 -0500

    fix(vsi): fixed swift support for vsi and added dynamic port numbers for health check

--- generators/vsi/templates/terraform/scripts/build_master.sh
+++ generators/vsi/templates/terraform/scripts/build_master.sh
@@ -11,13 +11,12 @@ mv setup.py setup.py.OLD
 pip install -r requirements.txt
 {{/has}}
 {{#has deployment.language 'SWIFT'}}
-swift build
+swift build -c release
 {{/has}}
 {{#has deployment.language 'SPRING'}}
 mvn clean install
 {{/has}}
 {{#has deployment.language 'JAVA'}}
 mvn clean install
-sed -i 's/default.http.port=9080/default.http.port=3000/g' target/liberty/wlp/usr/servers/defaultServer/bootstrap.properties
 target/liberty/wlp/bin/server package defaultServer --archive="{{deployment.name}}" --include=minify
 {{/has}}
