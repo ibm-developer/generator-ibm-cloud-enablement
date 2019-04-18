850fe4713f987a8c0c3e80131565c4a94fa36b4a
Andrew Trice <amtrice@us.ibm.com@Andrews-MacBook-Pro-3.local>
2019-04-18 15:03:35 -0400

    fix: java and node vsi installations were broken

--- generators/vsi/templates/terraform/scripts/install_master.sh
+++ generators/vsi/templates/terraform/scripts/install_master.sh
@@ -3,8 +3,9 @@
 apt-get update
 apt-get install -f
 {{#has deployment.language 'NODE'}}
-wget -qO- "https://deb.nodesource.com/setup_8.x" | bash -;
-apt-get install -y nodejs
+wget -qO- "https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh" | bash -;
+source ~/.profile
+nvm install 8.16.0
 {{/has}}
 {{#has deployment.language 'PYTHON'}}
 apt-get install -y python3-pip
